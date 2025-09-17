a2503077a0437390779d269709a3ddfa1a7869d3
Freddy Rosa <freddy.rosa16@gmail.com>
2025-09-17 14:11:59 -0400

    Implement load_tasks function to load tasks from a JSON file and handle empty or invalid files

---
+++ load_tasks.py
@@ -0,0 +1,27 @@
+import os
+import json
+from task_tracker_cli.file_path import get_task_file_path
+from task_tracker_cli.save import save_tasks
+
+def load_tasks():
+    file_path = get_task_file_path()
+
+    if not os.path.exists(file_path):
+        tasks = []
+        save_tasks(tasks)
+
+    elif os.path.getsize(file_path) == 0:
+        tasks = []
+        save_tasks(tasks)
+        
+    else:
+        try:
+            with open(file_path, 'r') as file:
+                tasks = json.load(file)
+                if not isinstance(tasks, list):
+                    tasks = []
+                    save_tasks(tasks)
+        except json.decoder.JSONDecodeError:
+            tasks = []
+            save_tasks(tasks)
+    return tasks
