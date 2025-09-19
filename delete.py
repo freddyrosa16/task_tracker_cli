61737ffcca30f0e33188caa383f81800097df2bd
Freddy Rosa <freddy.rosa16@gmail.com>
2025-09-18 22:45:25 -0400

    Implement delete function to remove tasks from the Todo Tracker

---
+++ delete.py
@@ -0,0 +1,25 @@
+import sys
+from save import save_tasks
+from load_tasks import load_tasks
+
+def delete():
+    tasks = load_tasks()
+    args = sys.argv[1:]
+    found = False
+
+    try:
+        argument_id = int(args[1])
+        for task in tasks:
+            if int(task["id"]) == argument_id:
+                found = True
+                tasks.remove(task)
+                save_tasks(tasks)
+                print("Task deleted successfully")
+                print(tasks)
+                break
+        if not found:
+            print("Task id not found.")
+    except ValueError:
+        print("Invalid input. Please enter a number (integer).")
+    except Exception as e:
+            print(f"An unexpected error occurred: {e}")
