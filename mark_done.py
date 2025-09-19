58ebd33f84c8521f07da080ab79243db4ef2126e
Freddy Rosa <freddy.rosa16@gmail.com>
2025-09-19 15:46:19 -0400

    Implement mark_in_progress function to update task status and handle errors

---
+++ mark_done.py
@@ -0,0 +1,27 @@
+import sys
+from save import save_tasks
+from load_tasks import load_tasks
+from datetime import datetime
+
+def mark_in_progress():
+    tasks = load_tasks()
+    args = sys.argv[1:]
+    found = False
+
+    try:
+        argument_id = int(args[1])
+        for task in tasks:
+            if int(task["id"]) == argument_id:
+                found = True
+                task["status"] = "done"
+                task["updatedAt"] = datetime.now().replace(minute=0, second=0, microsecond=0)
+                save_tasks(tasks)
+                print("Status has been updated successfully.")
+                print(tasks)
+                break
+        if not found:
+            print("Task id not found.")
+    except ValueError:
+        print("Invalid input. Please enter a number (integer).")
+    except Exception as e:
+            print(f"An unexpected error occurred: {e}")
