c855d643236b0c74807966b973e8ca6075beea95
Freddy Rosa <freddy.rosa16@gmail.com>
2026-01-21 13:50:41 -0400

    Refactor add function to improve task ID assignment and streamline task creation

--- add.py
+++ add.py
@@ -4,22 +4,26 @@ from save_tasks import save_tasks
 
 
 def add(tasks, path):
+    if len(sys.argv) < 3:
+        print("Usage: python3 main.py add <description>")
+        return
+
     if tasks:
-        id = max(task["id"] for task in tasks) + 1
+        task_id = max(task["id"] for task in tasks) + 1
     else:
-        id = 1
-    description = sys.argv[2]
-    status = "todo"
-    createdAt = datetime.datetime.now().isoformat()
-    updatedAt = datetime.datetime.now().isoformat()
+        task_id = 1
+
+    description = " ".join(sys.argv[2:])
+    now = datetime.datetime.now().isoformat()
 
     new_task = {
-        "id": id,
+        "id": task_id,
         "description": description,
-        "status": status,
-        "createdAt": createdAt,
-        "updatedAt": updatedAt,
+        "status": "todo",
+        "createdAt": now,
+        "updatedAt": now,
     }
+
     tasks.append(new_task)
     save_tasks(tasks, path)
     return new_task
