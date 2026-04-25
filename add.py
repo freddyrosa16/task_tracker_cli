1a04b7742e73a38df038abaf7cd41f6237d0cf22
freddyrosa16 <freddy.rosa16@gmail.com>
2026-04-25 18:35:39 -0400

    creating the add function for when the user writes add in the argument

---
+++ add.py
@@ -0,0 +1,28 @@
+import datetime
+import json
+import sys
+
+from load_tasks import load_tasks
+
+
+def add(file_path, args):
+    tasks = load_tasks(file_path)
+
+    new_id = max((task.get("id", 0) for task in tasks), default=0) + 1
+    description = args[1]
+    status = "todo"
+    createdAt = datetime.datetime.now().isoformat()
+    updatedAt = datetime.datetime.now().isoformat()
+
+    new_task = {
+        "id": new_id,
+        "description": description,
+        "status": status,
+        "createdAt": createdAt,
+        "updatedAt": updatedAt,
+    }
+
+    tasks.append(new_task)
+    with open(file_path, "w") as file:
+        json.dump(tasks, file, indent=4)
+    return tasks
