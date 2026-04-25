a0093830492fac21120c714c7998e7b3e8109721
freddyrosa16 <freddy.rosa16@gmail.com>
2026-04-25 18:35:58 -0400

    refractoring the tasks

--- load_tasks.py
+++ load_tasks.py
@@ -7,20 +7,24 @@ def load_tasks(file_path):
         if not os.path.exists(file_path):
             with open(file_path, "w") as file:
                 json.dump([], file)
-                return []
+            return []
         else:
             if os.path.getsize(file_path) == 0:
                 with open(file_path, "w") as file:
                     json.dump([], file)
-                    return []
+                return []
             else:
                 with open(file_path, "r") as file:
                     data = json.load(file)
-                    if not isinstance(data[0], list) or not isinstance(data[1], dict):
-                        with open(file_path, "w") as file:
-                            json.dump([], file)
-                            return []
-                    return data
+                if not isinstance(data, list):
+                    with open(file_path, "w") as file:
+                        json.dump([], file)
+                    return []
+                if not all(isinstance(task, dict) for task in data):
+                    with open(file_path, "w") as file:
+                        json.dump([], file)
+                    return []
+                return data
 
     except Exception as e:
-        return f"Error: {e}"
+        print(f"Error: {e}")
