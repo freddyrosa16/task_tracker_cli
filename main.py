1e4164ca72df08f973ef6fc326b8ac234ba65700
freddyrosa16 <freddy.rosa16@gmail.com>
2026-04-25 17:23:28 -0400

    Main function for the users usage

---
+++ main.py
@@ -0,0 +1,18 @@
+import sys
+
+from add import add
+
+
+def main():
+    file_path = "tasks.json"
+
+    args = sys.argv[1:]
+    if len(args) < 2:
+        print("Usage: python3 main.py <command> <id> <some description>")
+
+    if args[0] == "add":
+        print(add(file_path))
+
+
+if "__main__" == __name__:
+    main()
