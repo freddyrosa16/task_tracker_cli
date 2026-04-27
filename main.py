07469e5b049841027042457c4a42c837bb95d62d
freddyrosa16 <freddy.rosa16@gmail.com>
2026-04-27 11:41:57 -0400

    adding logic for mark done

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import sys
 
 from add import add
+from mark_done import mark_done
+
+# from delete import delete
+# from lists import lists
+from mark_in_progress import mark_in_progress
+from update import update
 
 
 def main():
@@ -12,6 +18,12 @@ def main():
 
     if args[0] == "add":
         print(add(file_path, args))
+    if args[0] == "update":
+        print(update(file_path, args))
+    if args[0] == "mark-in-progress":
+        print(mark_in_progress(file_path, args))
+    if args[0] == "mark-done":
+        print(mark_done(file_path, args))
 
 
 if "__main__" == __name__:
